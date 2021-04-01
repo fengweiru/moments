2ca3c4b4d9ab528d52208e3fdc808487c421111c
冯伟如 <fengwr@zjtoman.com>
2021-04-01 08:19:58 +0800

    init

---
+++ moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moments
+//
+//  Created by 冯伟如 on 2021/3/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
